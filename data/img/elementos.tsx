9aff76b0bd40f4eeff480101368086ebc3f8b55c
ttatiana18 <61387311+ttatiana18@users.noreply.github.com>
2020-12-10 16:47:34 -0500

    archivos para el mapa

---
+++ data/img/elementos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="elementos" tilewidth="16" tileheight="16" tilecount="924" columns="33">
+ <image source="Estudio/semestre 6/Computacion grafica/proyecto/img/elementos.png" width="528" height="448"/>
+</tileset>
