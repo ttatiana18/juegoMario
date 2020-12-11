0d52999fe71e5cbe4b7e459e106944c223fe42ac
Sanss <santi-2000-12@hotmail.com>
2020-12-10 23:47:37 -0500

    new mapa3.png

--- data/img/elementos.tsx
+++ data/img/elementos.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="elementos" tilewidth="16" tileheight="16" tilecount="924" columns="33">
- <image source="Estudio/semestre 6/Computacion grafica/proyecto/img/elementos.png" width="528" height="448"/>
+<tileset version="1.4" tiledversion="2020.10.30" name="elementos" tilewidth="16" tileheight="16" tilecount="2016" columns="72">
+ <image source="elementos2.png" width="1152" height="448"/>
 </tileset>
--- data/img/elementos2.tsx
+++ data/img/elementos2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="elementos" tilewidth="32" tileheight="32" tilecount="924" columns="33">
- <image source="Mapa-v4/Mapa/data/img/elementos_mapa.png" width="1056" height="896"/>
+<tileset version="1.4" tiledversion="2020.10.30" name="elementos" tilewidth="32" tileheight="32" tilecount="924" columns="33">
+ <image source="elementos_mapa.png" width="1056" height="896"/>
 </tileset>
